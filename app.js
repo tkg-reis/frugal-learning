f63ed733d6fb72c01fcd5ea042aacee9881f3f64
Masataka Takaki <oorgns1uw5sk3ae@gmail.com>
2024-01-20 21:19:32 +0900

    create pathparameter , routing

--- app.js
+++ app.js
@@ -2,13 +2,26 @@ const port = process.env.PORT | 3000;
 const express = require('express');
 const path = require('path');
 const favicon = require("serve-favicon");
+const fs = require('fs');
+
+const bodyParser = require('body-parser');
+const logger = require('./lib/log/logger.js');
+const applicationLogger = require("./lib/log/applicationlogger.js");
+const accessLogger = require("./lib/log/accessLogger.js");
 const app = express();
 
 // テンプレートエンジンの有効化
 app.set('view engine' , 'ejs');
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+
+// サーバーサイドの隠蔽
+app.disable("x-powered-by");
 
 // favionの配置
 app.use(favicon(path.join(__dirname , 'public' , 'images' , 'favicon.ico')));
+
+// app.use(accessLogger());
 
 // 使用するサーバーの選択
 app.use("/" , require("./routes/index.js"));
@@ -19,5 +32,5 @@ app.use("/public" , express.static(path.join(__dirname, '/public')));
 
 // ローカルサーバーを建てる
 app.listen(port , () => {
-    console.log("port running is " + port);
+    "port running is " + port;
 });
